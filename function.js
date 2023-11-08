be0fa9f55839d33c0ae69094792444e6b177764a
Krenner Dominik <99679489+pityung@users.noreply.github.com>
2023-11-08 12:58:42 +0100

    container
    img containers added and thats gonna be a link to my shop page

--- function.js
+++ function.js
@@ -2,12 +2,11 @@ function Darkmode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
     if (element.classList == "dark-mode") {
-        document.getElementById("darkmode").src = "logoDark.jpg" ;
+        document.getElementById("darkmode").src = "logoDark.jpg";
         document.getElementById("navbar").style.backgroundColor = "white";
-    }else{
-        document.getElementById("darkmode").src = "logo.jpg" ;
+    } else {
+        document.getElementById("darkmode").src = "logo.jpg";
         document.getElementById("navbar").style.backgroundColor = "#2d2d30";
-
     }
- }
+}
 
