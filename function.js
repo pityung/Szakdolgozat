3d82d26d22d62eb9f5d9e71b18fcbb8658cdc4de
Krenner Dominik <99679489+pityung@users.noreply.github.com>
2023-11-08 19:49:30 +0100

    Shop
    created Shop.html .css .js from an online theme. moved all images to a new folder called "images"

--- function.js
+++ function.js
@@ -2,10 +2,10 @@ function Darkmode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
     if (element.classList == "dark-mode") {
-        document.getElementById("darkmode").src = "logoDark.jpg";
+        document.getElementById("darkmode").src = "images/logoDark.jpg";
         document.getElementById("navbar").style.backgroundColor = "white";
     } else {
-        document.getElementById("darkmode").src = "logo.jpg";
+        document.getElementById("darkmode").src = "images/logo.jpg";
         document.getElementById("navbar").style.backgroundColor = "#2d2d30";
     }
 }
