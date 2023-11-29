cfbf799ff8f8581473a4d9e378091a2af555e323
Krenner Dominik <99679489+pityung@users.noreply.github.com>
2023-11-29 13:55:38 +0100

    slides
    created all the slide bars to shop.html, the next big step is to delet all html elments and create php's instead

--- scripts/shopScript.js
+++ scripts/shopScript.js
@@ -9,19 +9,6 @@ function jeansFunc() {
   }
 }
 
-// Open and close sidebar
-function w3_open() {
-  document.getElementById("mySidebar").style.display = "block";
-  document.getElementById("myOverlay").style.display = "block";
-}
- 
-function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
-  document.getElementById("myOverlay").style.display = "none";
-}
-
-
-
 // Accordion 
 function jacketFunc() {
   var x = document.getElementById("jackets");
@@ -32,6 +19,48 @@ function jacketFunc() {
   }
 }
 
+// Accordion 
+function gymWearFunc() {
+  var x = document.getElementById("gymWear");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function dressFunc() {
+  var x = document.getElementById("dress");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function shirtFunc() {
+  var x = document.getElementById("shirt");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function blazerFunc() {
+  var x = document.getElementById("blazer");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function shoesFunc() {
+  var x = document.getElementById("shoes");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
 // Open and close sidebar
 function w3_open() {
   document.getElementById("mySidebar").style.display = "block";
